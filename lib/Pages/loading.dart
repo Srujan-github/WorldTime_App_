977eacc5f51546ba0191c9795b48e33ac2cce02e
Srujan-github <101975044+Srujan-github@users.noreply.github.com>
2022-09-26 22:57:48 +0530

    Loding page is updated

--- lib/Pages/loading.dart
+++ lib/Pages/loading.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:worldtimeapp/Services/World_Time.dart';
 class Loading extends StatefulWidget {
@@ -6,6 +8,7 @@ class Loading extends StatefulWidget {
   @override
   State<Loading> createState() => _LoadingState();
 }
+
 
 class _LoadingState extends State<Loading> {
 
@@ -29,11 +32,50 @@ class _LoadingState extends State<Loading> {
   }
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      body:Padding(
-        padding: EdgeInsets.all(30.0),
-        child: Text('Loding'),
-      )
+    return MaterialApp(
+      title: 'Splash Screen',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: MyHomePage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(Duration(seconds: 3),
+            ()=>Navigator.pushReplacement(context,
+            MaterialPageRoute(builder:
+                (context) =>
+                SecondScreen()
+            )
+        )
+    );
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        color: Colors.white,
+        child:FlutterLogo(size:MediaQuery.of(context).size.height)
+    );
+  }
+}
+class SecondScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title:Text("GeeksForGeeks")),
+      body: Center(
+          child:Text("Home page",textScaleFactor: 2,)
+      ),
     );
   }
 }
